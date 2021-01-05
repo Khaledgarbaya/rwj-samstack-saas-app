344f67e473a06bbafaaef952e1ac4fe639605de9
Khaled Garbaya <kgarbaya@gmail.com>
2021-01-05 16:59:33 +0100

    02-finish

---
+++ src/components/login.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react'
+import {navigate} from 'gatsby'
+import {IdentityContext} from '../../identity-context'
+
+const Login = () => {
+  const {identity: netlifyIdentity} = useContext(IdentityContext)
+  netlifyIdentity.on('login', (user) => {
+    navigate('/app/dashboard')
+  })
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button
+        onClick={() => {
+          netlifyIdentity.open()
+        }}
+      >
+        Log In
+      </button>
+    </div>
+  )
+}
+
+export default Login
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Router} from '@reach/router'
+import PrivateRoute from '../components/private-route'
+import Login from '../components/login'
 
-const Login = () => <h1>Login Page</h1>
 const Dash = () => <h1>Dashboard Page</h1>
 const Default = () => <h1>App Page</h1>
+
 const App = () => {
   return (
     <>
       <Router basepath="/app">
-        <Dash path="/dashboard" />
+        <PrivateRoute component={Dash} path="/dashboard" />
         <Login path="/login" />
         <Default path="/" />
       </Router>
