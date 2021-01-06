f1cdd0aff6f2f3d28cd8e8454e5cba504889b4f9
Khaled Garbaya <kgarbaya@gmail.com>
2021-01-06 19:50:13 +0100

    add stripe customer id

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,9 +1,23 @@
 const fetch = require('node-fetch')
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 exports.handler = async (event) => {
   const {user} = JSON.parse(event.body)
 
-  // store the Netlify user in Hasura
+  // create a stripe customer
+  const customer = stripe.customers.create({
+    email: user.email,
+  })
+
+  // assign free subscription
+  await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [
+      {
+        price: process.env.FREE_PLAN_PRICE,
+      },
+    ],
+  })
+
   try {
     const data = await fetch(process.env.HASURA_URL, {
       method: 'POST',
@@ -13,8 +27,8 @@ exports.handler = async (event) => {
       },
       body: JSON.stringify({
         query: `
-        mutation UserMutation($email: String, $user_id: String) {
-          insert_user(objects: {email: $email, user_id: $user_id}){
+        mutation UserMutation($email: String, $user_id: String, $stripe_customer_id: String) {
+          insert_user(objects: {email: $email, user_id: $user_id, stripe_customer_id: $stripe_customer_id}) {
             affected_rows
           }
         }
@@ -22,12 +36,12 @@ exports.handler = async (event) => {
         variables: {
           email: user.email,
           user_id: user.id,
+          stripe_customer_id: customer.id,
         },
       }),
     }).then((res) => res.json())
-    console.log(data)
   } catch (e) {
-    console.error(JSON.stringify(err, null, 2))
+    console.error(JSON.stringify(e), null, 2)
     return {
       statusCode: 500,
     }
@@ -37,7 +51,7 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       app_metadata: {
-        roles: ['free'],
+        role: ['free'],
       },
     }),
   }
